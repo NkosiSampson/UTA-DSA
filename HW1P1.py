e6084ace236d67a81f9c1d244ed66221a6955f8c
nkosisampson <nkosisampson11@gmail.com>
2025-09-05 13:50:26 -0700

    structure done

---
+++ HW1P1.py
@@ -0,0 +1,61 @@
+"""Assignment 1: Friend of a Friend
+
+Please complete these functions, to answer queries given a dataset of
+friendship relations, that meet the specifications of the handout
+and docstrings below.
+
+Notes:
+- you should create and test your own scenarios to fully test your functions, 
+  including testing of "edge cases"
+"""
+
+
+"""
+************** READ THIS ***************
+************** READ THIS ***************
+************** READ THIS ***************
+************** READ THIS ***************
+************** READ THIS ***************
+
+If you worked in a group on this project, please type the EIDs of your groupmates below (do not include yourself).
+Leave it as TODO otherwise.
+Groupmate 1: TODO
+Groupmate 2: TODO
+"""
+
+
+def load_pairs(filename):
+    """
+    Args:
+        filename (str): name of input file
+
+    Returns:
+        List of pairs, where each pair is a Tuple of two strings
+
+    Notes:
+    - Each non-empty line in the input file contains two strings, that
+      are separated by one or more space characters.
+    - You should remove whitespace characters, and skip over empty input lines.
+    """
+    list_of_pairs = []
+    with open(filename, 'rt') as infile:
+        # ------------ BEGIN YOUR CODE ------------
+        with open("friends-NkosiSampson/myfriends.txt", "rt") as infile:
+            for line in infile:
+                if line == "\n":
+                    pass
+                else:
+                    list_of_pairs.append((line.split()))
+        # ------------ END YOUR CODE ------------
+    return list_of_pairs
+#print(load_pairs("friends-NkosiSampson/myfriends.txt"))
+directory = dict()
+
+
+# ------------ BEGIN YOUR CODE ------------
+
+
+
+# ------------ END YOUR CODE ------------
+
+
---
+++ HW1P2.py
@@ -0,0 +1,33 @@
+def make_friends_directory(pairs):
+    """Create a directory of persons, for looking up immediate friends
+
+    Args:
+        pairs (List[Tuple[str, str]]): list of pairs
+
+    Returns:
+        Dict[str, Set] where each key is a person, with value being the set of
+        related persons given in the input list of pairs
+
+    Notes:
+    - you should infer from the input that relationships are two-way:
+      if given a pair (x,y), then assume that y is a friend of x, and x is
+      a friend of y
+    - no own-relationships: ignore pairs of the form (x, x)
+    """
+
+
+directory = dict()
+list = [['Tom', 'Jerry'], ['Tim', 'Bo'], ['Tom', 'Bo']]
+for duple in list:
+    for name in duple:
+        if name not in directory.keys():
+            directory[name] = 1
+        elif name in directory.keys():
+            directory[name] += 1
+print(directory)
+
+
+
+    # ------------ END YOUR CODE ------------
+
+  #  return directory
